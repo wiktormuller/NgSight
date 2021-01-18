ff895ad3a780d8669ab3c48c9a5d6b6ca95f5047
Wiktor Muller <biznes.wiktor@gmail.com>
2021-01-18 11:13:49 +0100

    Add pie chart component data

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { bindCallback } from 'rxjs';
 
 @Component({
   selector: 'app-pie-chart',
@@ -8,6 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class PieChartComponent implements OnInit {
 
   constructor() { }
+
+  pieChartData: number[] = [350, 450, 120];
+  pieChartLabels: string[] = ['XYZ Logistics', 'Main St bakery', 'Acme Hosting'];
+  colors: any[] = [
+    {
+      backgroundColor: ['#26547c', '#ff6b6b', '#ffd166'],
+      borderColor: '#111'
+    }
+  ];
+
+  pieChartType = 'doughnut';
 
   ngOnInit(): void {
   }
